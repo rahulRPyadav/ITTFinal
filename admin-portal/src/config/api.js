895f287eec9c5a55fc7bc5f3d30fe7886a468991
ranandyadavAp <ranandyadav.tech@hotmail.com>
2026-09-21 11:04:38 +0530

    Urls Added

--- admin-portal/src/config/api.js
+++ admin-portal/src/config/api.js
@@ -1,5 +1,6 @@
- export const API_BASE_URL = 'http://localhost:5000';
-// export const API_BASE_URL = 'https://ishika-deploy.onrender.com';
+//  export const API_BASE_URL = 'http://localhost:5000';
+export const API_BASE_URL = 'https://ittfinal.onrender.com/';
+
 
 
 // Customer Website Frontend URL (Local vs Live)
