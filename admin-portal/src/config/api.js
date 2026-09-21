efffc3ff83c5eb3993afe3a80f83fc1d113ddf13
ranandyadavAp <ranandyadav.tech@hotmail.com>
2026-09-21 11:11:29 +0530

    Fixing

--- admin-portal/src/config/api.js
+++ admin-portal/src/config/api.js
@@ -1,5 +1,5 @@
 //  export const API_BASE_URL = 'http://localhost:5000';
-export const API_BASE_URL = 'https://ittfinal.onrender.com/';
+export const API_BASE_URL = 'https://ittfinal.onrender.com';
 
 
 
